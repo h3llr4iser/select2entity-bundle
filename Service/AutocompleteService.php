00fbdff249208a7c5c91195f740f958e432ac116
Sergio gallego <sgallego@dfutura.com>
2017-12-23 10:49:10 +0100

    modify callback query

--- Service/AutocompleteService.php
+++ Service/AutocompleteService.php
@@ -11,78 +11,81 @@ use Symfony\Component\PropertyAccess\PropertyAccess;
 
 class AutocompleteService
 {
-    /**
-     * @var FormFactoryInterface
-     */
-    private $formFactory;
+	/**
+	 * @var FormFactoryInterface
+	 */
+	private $formFactory;
 
-    /**
-     * @var ManagerRegistry
-     */
-    private $doctrine;
+	/**
+	 * @var ManagerRegistry
+	 */
+	private $doctrine;
 
-    /**
-     * @param FormFactoryInterface $formFactory
-     * @param ManagerRegistry      $doctrine
-     */
-    public function __construct(FormFactoryInterface $formFactory, ManagerRegistry $doctrine)
-    {
-        $this->formFactory = $formFactory;
-        $this->doctrine = $doctrine;
-    }   
+	/**
+	 * @param FormFactoryInterface $formFactory
+	 * @param ManagerRegistry      $doctrine
+	 */
+	public function __construct(FormFactoryInterface $formFactory, ManagerRegistry $doctrine)
+	{
+		$this->formFactory = $formFactory;
+		$this->doctrine = $doctrine;
+	}
 
-    /**
-     * @param Request                  $request
-     * @param string|FormTypeInterface $type
-     *
-     * @return array
-     */
-    public function getAutocompleteResults(Request $request, $type)
-    {
-        $form = $this->formFactory->create($type);
-        $fieldOptions = $form->get($request->get('field_name'))->getConfig()->getOptions();
+	/**
+	 * @param Request                  $request
+	 * @param string|FormTypeInterface $type
+	 *
+	 * @return array
+	 */
+	public function getAutocompleteResults(Request $request, $type)
+	{
+		$form = $this->formFactory->create($type);
+		$fieldOptions = $form->get($request->get('field_name'))->getConfig()->getOptions();
 
-        /** @var EntityRepository $repo */
-        $repo = $this->doctrine->getRepository($fieldOptions['class']);
+		/** @var EntityRepository $repo */
+		$repo = $this->doctrine->getRepository($fieldOptions['class']);
 
-        $term = $request->get('q');
+		$term = $request->get('q');
 
-        $countQB = $repo->createQueryBuilder('e');
-        $countQB
-            ->select($countQB->expr()->count('e'))
-            ->where('e.'.$fieldOptions['property'].' LIKE :term')
-            ->setParameter('term', '%' . $term . '%')
-        ;
+		$countQB = $repo->createQueryBuilder('e');
+		$countQB->select($countQB->expr()->count('e'));
+		$resultQb = $repo->createQueryBuilder('e');
 
-        $maxResults = $fieldOptions['page_limit'];
-        $offset = ($request->get('page', 1) - 1) * $maxResults;
 
-        $resultQb = $repo->createQueryBuilder('e');
-        $resultQb
-            ->where('e.'.$fieldOptions['property'].' LIKE :term')
-            ->setParameter('term', '%' . $term . '%')
-            ->setMaxResults($maxResults)
-            ->setFirstResult($offset)
-        ;
+		if (is_callable($fieldOptions['callback'])) {
+			$cb = $fieldOptions['callback'];
 
-        if (is_callable($fieldOptions['callback'])) {
-            $cb = $fieldOptions['callback'];
+			$cb($countQB, $request);
+			$cb($resultQb, $request);
+		}else{
+			$countQB
+				->where('e.'.$fieldOptions['property'].' LIKE :term')
+				->setParameter('term', '%' . $term . '%')
+			;
 
-            $cb($countQB, $request);
-            $cb($resultQb, $request);
-        }
+			$resultQb
+				->where('e.'.$fieldOptions['property'].' LIKE :term')
+				->setParameter('term', '%' . $term . '%')
 
-        $count = $countQB->getQuery()->getSingleScalarResult();
-        $paginationResults = $resultQb->getQuery()->getResult();
+			;
+		}
+		$count = $countQB->getQuery()->getSingleScalarResult();
+		$maxResults = $fieldOptions['page_limit'];
+		$offset = ($request->get('page', 1) - 1) * $maxResults;
+		$resultQb->setMaxResults($maxResults)->setFirstResult($offset);
 
-        $result = ['results' => null, 'more' => $count > ($offset + $maxResults)];
 
-        $accessor = PropertyAccess::createPropertyAccessor();
+		$paginationResults = $resultQb->getQuery()->getResult();
 
-        $result['results'] = array_map(function ($item) use ($accessor, $fieldOptions) {
-            return ['id' => $accessor->getValue($item, $fieldOptions['primary_key']), 'text' => $accessor->getValue($item, $fieldOptions['property'])];
-        }, $paginationResults);
+		$result = ['results' => null, 'more' => $count > ($offset + $maxResults)];
 
-        return $result;
-    }
+		$accessor = PropertyAccess::createPropertyAccessor();
+
+
+		$result['results'] = array_map(function ($item) use ($accessor, $fieldOptions) {
+			return ['id' => $accessor->getValue($item, $fieldOptions['primary_key']), 'text' => $accessor->getValue($item, $fieldOptions['property'])];
+		}, $paginationResults);
+
+		return $result;
+	}
 }
